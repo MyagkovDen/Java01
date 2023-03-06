aa85e298e5dfee0d008775913924d7728f0b1134
Denis Myagkov <tomich-84@mail.ru>
2023-03-06 15:10:02 +0300

    task01 correction of mistakes

--- task01/task01.java
+++ task01/task01.java
@@ -6,20 +6,21 @@ public class task01 {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
         System.out.print("Введите число: ");
-        boolean flag = iScanner.hasNextInt();
-        System.out.println(flag);
+        // boolean flag = iScanner.hasNextInt();
+        // System.out.println(flag);
         int n = iScanner.nextInt();
         iScanner.close();
 
-        int sum = 0;
-        int factorial = 1;
-
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-            factorial *= i;
+        if (n > 0) {
+            int sum = 0;
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+                factorial *= i;
+            }
+            System.out.printf("n-е треугольного число %d = %d\nфакториал %d = %d", n, sum, n, factorial);
+        } else {
+            System.out.println("Ошибка ввода!");
         }
-
-        System.out.printf("n-е треугольного число %d = %d\nфакториал %d = %d", n, sum, n, factorial);
-
     }
 }
---
+++ task01/task04_05.java
@@ -0,0 +1,35 @@
+// Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, 
+// например 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или 
+//сообщить, что его нет.
+
+import java.util.Scanner;
+
+public class task04_05 {
+
+    public static void main(String[] args) {
+
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите уравнение: ");
+        String equation = iScanner.nextLine();
+        iScanner.close();
+
+        String[] parts = equation.split(" ");
+        String arg1 = parts[0];
+        String arg2 = parts[2];
+        int c = Integer.parseInt(parts[4]);
+
+        for (int i = 0; i < 10; i++) {
+            String s1 = Integer.toString(i);
+            String replArg1 = arg1.replace("?", s1);
+            for (int j = 0; j < 10; j++) {
+                String s2 = Integer.toString(j);
+                String replArg2 = arg2.replace("?", s2);
+                int a = Integer.parseInt(replArg1);
+                int b = Integer.parseInt(replArg2);
+                if (a + b == c) {
+                    System.out.printf("%d + %d = %d", a, b, c);
+                }
+            }
+        }
+    }
+}
