3f77d870cef0b7ba5419815990b8059ce29e9111
Denis Myagkov <tomich-84@mail.ru>
2023-03-06 15:20:22 +0300

    task02 correction of mictakes

--- task02/task02.java
+++ task02/task02.java
@@ -6,7 +6,7 @@ public class task02 {
 
         for (int i = 1; i < 1001; i++) {
             boolean flag = true;
-            for (int j = 2; j < i; j++) {
+            for (int j = 2; j < i / 2 + 1; j++) {
                 if (i % j == 0) {
                     flag = false;
                     break;
